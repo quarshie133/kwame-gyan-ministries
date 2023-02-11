891c112d7ffe4bea10aeb691527d292071a8d3d4
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-02-11 16:40:20 +0000

    mainbackground image

--- src/components/pages/MainSection.jsx
+++ src/components/pages/MainSection.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "../styles/MainSection.css";
-
+import mainbackground from "../../assets/mainbackground.png"
 
 function MainSection() {
   return (
-    <div className="">
-      <h1>--main section--</h1>
+    <div className="main-section">
+      <img src={mainbackground} />
     </div>
   );
 }
