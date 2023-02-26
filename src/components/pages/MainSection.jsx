22296c2b98f0366d07d6a7fd5cc89098af72baa1
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-02-26 14:41:29 +0000

    added main background

--- src/components/pages/MainSection.jsx
+++ src/components/pages/MainSection.jsx
@@ -1,13 +1,44 @@
 import React from "react";
 import "../styles/MainSection.css";
-import mainbackground from "../../assets/mainbackground.png"
+import background from "../../assets/mainbackground.png";
 
 function MainSection() {
   return (
-    <div className="main-section">
-      <img src={mainbackground} />
+    <div className="img-container">
+      <div className="container">
+        <div className="row text center">
+          <div className="col-lg-12">
+            <div className="text-drop">
+            
+                <h3>Mandate</h3>
+                <div className="underline"></div>
+              
+              <p>
+                Our mandate is through music we are to bring individuals whose
+                hearts are distant from God back to Him, and to reignite the
+                love of Jesus Christ in the hearts of many, particularly young
+                people in partnership with the Holyspirit. Through the power of
+                music, we strive to manifest the healing power of God and bring
+                hope and salvation to all who seek it.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default MainSection;
+
+/*
+       <p>
+          Our mandate is through music we are to bring individuals whose hearts
+          are distant from God back to Him, and to reignite the love of Jesus
+          Christ in the hearts of many, particularly young people in partnership
+          with the Holyspirit. Through the power of music, we strive to manifest
+          the healing power of God and bring hope and salvation to all who seek
+          it.
+        </p>
+          
+*/
