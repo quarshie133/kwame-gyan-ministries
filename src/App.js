cada688e92df5cb324c104f7a1e2fac14c537810
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-03-12 15:54:24 +0000

    main section layout styling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <NavHeroSection />
       <MainSection />
+      
     </div>
   );
 }
--- src/components/pages/MainSection.jsx
+++ src/components/pages/MainSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/MainSection.css";
-import background from "../../assets/mainbackground.png";
+import group1 from "../../assets/group1.png";
+import group2 from "../../assets/group2.png";
 
 function MainSection() {
   return (
@@ -9,11 +10,10 @@ function MainSection() {
         <div className="row text center">
           <div className="col-lg-12">
             <div className="text-drop">
-            
-                <h3>Mandate</h3>
-                <div className="underline"></div>
-              
-              <p>
+              <h3>Mandate</h3>
+              <div className="underline"></div>
+
+              <p className="txt-center">
                 Our mandate is through music we are to bring individuals whose
                 hearts are distant from God back to Him, and to reignite the
                 love of Jesus Christ in the hearts of many, particularly young
@@ -21,24 +21,15 @@ function MainSection() {
                 music, we strive to manifest the healing power of God and bring
                 hope and salvation to all who seek it.
               </p>
+              <button className="btn-read-more">Read more</button>
             </div>
           </div>
         </div>
       </div>
+            <img src={group1} className="grp_1"/>
+            <img src={group2} className="grp_2"/>
     </div>
   );
 }
 
 export default MainSection;
-
-/*
-       <p>
-          Our mandate is through music we are to bring individuals whose hearts
-          are distant from God back to Him, and to reignite the love of Jesus
-          Christ in the hearts of many, particularly young people in partnership
-          with the Holyspirit. Through the power of music, we strive to manifest
-          the healing power of God and bring hope and salvation to all who seek
-          it.
-        </p>
-          
-*/
