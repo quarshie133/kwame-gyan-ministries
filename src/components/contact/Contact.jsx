993265ddd48115701a6344f970407e5e595cfb03
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-08-18 14:27:50 +0000

    contact-us styling section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,13 +11,20 @@ import youtube from "../../assets/youtube.png";
 function Contact() {
   return (
     <div className="contact-bg w-100 d-flex">
-      <h2>Contact Us</h2>
-      <div className="contact-line"></div>
+    
+        <div className="subtle">
+          <h2 className="txt-subtitle">Contact Us</h2>
+          <div className="contact-line"></div>
+        </div>
+    
+
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
             <img src={telephone} className="contact__option-icon icon-1" />
-            0501363509 / 0248476820
+            <a href="#">
+              <h6 className="contact"> 0501363509 / 0248476820</h6>
+            </a>
           </article>
           <article className="contact__option">
             <img src={email} className="contact__option-icon icon-two" />
@@ -65,8 +72,18 @@ function Contact() {
         {/* END OF CONTACT OPTIONS */}
         <form>
           <input type="text" name="name" placeholder="Name" required />
+
           <input type="email" name="message" placeholder="Email" required />
-          <textarea name="message" id="" rows="7" required></textarea>
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            id=""
+            rows="7"
+            required
+          ></textarea>
+          <button type="submit" className="btn-primary">
+            SEND MESSAGE
+          </button>
         </form>
       </div>
     </div>
